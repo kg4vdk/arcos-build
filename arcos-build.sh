f79b270e2dbd20f3ab1c2800fcdb1326b512146b
Mike Fisher <mike@kg4vdk.com>
2025-11-24 17:16:21 +0000

    place arcos-build-key.pub in /tmp/arcos-build-key.pub, and add as authorized key file

--- arcos-build.sh
+++ arcos-build.sh
@@ -724,7 +724,6 @@ fi
 
 cleanup () {
 echo -e "${CYN}### CLEANUP ###${RST}" >&2
-ssh -i ${SSH_KEY} ${BUILD_USER}@${BUILD_HOST} "rm $HOME/.ssh/authorized_build_keys"
 sudo rm /etc/apt/sources.list.d/deb-src.list
 sudo rm /etc/apt/sources.list.d/mozillateam-ppa-noble.list
 sudo rm /etc/apt/sources.list.d/abcd567a.list
